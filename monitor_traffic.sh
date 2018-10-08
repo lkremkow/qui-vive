d44fbf5517b08e7d0793ee72a3bbb605f27e7619
Leif Kremkow <kremkow@tftg.net>
2018-10-08 13:55:05 +0200

    buffer events and upload in bulk

--- monitor_traffic.sh
+++ monitor_traffic.sh
@@ -4,7 +4,8 @@ source settings.sh
 
 # be sure to replace X.X.X.X with the IP of the monitoring sytems itself
 
-sudo tcpdump -l -tt --interface=any -nn -q 'tcp[tcpflags] & 18 = 2 and not host X.X.X.X' |
+#sudo tcpdump -l -tt --interface=any -nn -q 'tcp[tcpflags] & 18 = 2 and not host X.X.X.X' |
+sudo tcpdump -l -tt --interface=any -nn -q 'tcp[tcpflags] & 18 = 2' |
 while read -r line
 do
   cleaned_line=$( echo "$line" | sed 's/[\.:]/ /g' )
@@ -72,20 +73,42 @@ do
 
   #logger -p local0.6 "$time_stamp $from_ip,$from_ip_fqdn,$from_ip_netbios,$from_ip_known_hostile,$from_port,$from_ip_vulnerabilities_on_port || $to_ip,$to_ip_fqdn,$to_ip_netbios,$to_ip_known_hostile,$to_port,$to_ip_vulnerabilities_on_port" &
   #echo "$time_stamp_epoch $from_ip,$from_ip_fqdn,$from_ip_netbios,$from_ip_known_hostile,$from_port,$from_ip_vulnerabilities_on_port || $to_ip,$to_ip_fqdn,$to_ip_netbios,$to_ip_known_hostile,$to_port,$to_ip_vulnerabilities_on_port"
-  curl -H "Content-Type: application/json" -X 'POST' "https://snfah5i2mn:r9u5icf97@tftg-qui-vive-565163666.eu-central-1.bonsaisearch.net/qui_vive_index/doc" -d "{
-    \"time_stamp\": \"$time_stamp_epoch\",
-    \"from_ip\": \"$from_ip\",
-    \"from_ip_fqdn\": \"$from_ip_fqdn\",
-    \"from_ip_netbios\": \"$from_ip_netbios\",
-    \"from_ip_known_hostile\": \"$from_ip_known_hostile\",
-    \"from_port\": \"$from_port\",
-    \"from_ip_vulnerabilities_on_port\": \"$from_ip_vulnerabilities_on_port\",
-    \"to_ip\": \"$to_ip\",
-    \"to_ip_fqdn\": \"$to_ip_fqdn\",
-    \"to_ip_netbios\": \"$to_ip_netbios\",
-    \"to_ip_known_hostile\": \"$to_ip_known_hostile\",
-    \"to_port\": \"$to_port\",
-    \"to_ip_vulnerabilities_on_port\": \"$to_ip_vulnerabilities_on_port\"
-  }"
+
+  new_log_entry="{ \"time_stamp\": \"$time_stamp_epoch\", \"from_ip\": \"$from_ip\", \"from_ip_fqdn\": \"$from_ip_fqdn\", \"from_ip_netbios\": \"$from_ip_netbios\", \"from_ip_known_hostile\": \"$from_ip_known_hostile\", \"from_port\": \"$from_port\", \"from_ip_vulnerabilities_on_port\": \"$from_ip_vulnerabilities_on_port\", \"to_ip\": \"$to_ip\", \"to_ip_fqdn\": \"$to_ip_fqdn\", \"to_ip_netbios\": \"$to_ip_netbios\", \"to_ip_known_hostile\": \"$to_ip_known_hostile\", \"to_port\": \"$to_port\", \"to_ip_vulnerabilities_on_port\": \"$to_ip_vulnerabilities_on_port\" }"
+
+  multiple_log_entries="${multiple_log_entries}\n{ \"index\":{} }\n${new_log_entry}"
+
+  if [[ ${#multiple_log_entries} -ge 12000 ]]
+  then
+    echo "number_of_log_entries exceeding threshold; uploading to elastic"
+    multiple_log_entries="${multiple_log_entries}\n"
+    echo -e "$multiple_log_entries" > "$TMP"/log_buffer.txt
+    multiple_log_entries=""
+    curl --silent -H "Content-Type: application/json" -X 'POST' "https://snfah5i2mn:r9u5icf97@tftg-qui-vive-565163666.eu-central-1.bonsaisearch.net/qui_vive_index/doc/_bulk?pretty" --data-binary @"$TMP"/log_buffer.txt --output "$TMP"/monitor_elastic_update_log.txt &    
+  fi
+
+  echo "number_of_log_entries=${#multiple_log_entries}"
+
+  #echo -e "$multiple_log_entries" > log_file.txt
+
+
+
+
+  # curl -H "Content-Type: application/json" -X 'POST' "https://snfah5i2mn:r9u5icf97@tftg-qui-vive-565163666.eu-central-1.bonsaisearch.net/qui_vive_index/doc" -d "{
+  #   \"time_stamp\": \"$time_stamp_epoch\",
+  #   \"from_ip\": \"$from_ip\",
+  #   \"from_ip_fqdn\": \"$from_ip_fqdn\",
+  #   \"from_ip_netbios\": \"$from_ip_netbios\",
+  #   \"from_ip_known_hostile\": \"$from_ip_known_hostile\",
+  #   \"from_port\": \"$from_port\",
+  #   \"from_ip_vulnerabilities_on_port\": \"$from_ip_vulnerabilities_on_port\",
+  #   \"to_ip\": \"$to_ip\",
+  #   \"to_ip_fqdn\": \"$to_ip_fqdn\",
+  #   \"to_ip_netbios\": \"$to_ip_netbios\",
+  #   \"to_ip_known_hostile\": \"$to_ip_known_hostile\",
+  #   \"to_port\": \"$to_port\",
+  #   \"to_ip_vulnerabilities_on_port\": \"$to_ip_vulnerabilities_on_port\"
+  # }"
+  echo "have data to send to DB"
 
 done
